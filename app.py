9d088780f319765c1b7463830088baa416c83b08
Your Name <you@example.com>
2025-08-01 16:29:16 +0800

    Fix binding to 0.0.0.0 for Render

--- app.py
+++ app.py
@@ -135,5 +135,5 @@ def logout():
 
 # Run app
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(debug=True, host="0.0.0.0", port=port)
